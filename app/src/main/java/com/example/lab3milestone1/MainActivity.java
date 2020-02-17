21e58be44048b6153f55a02c30c2de282853a697
laimaster <cyuan42@wisc.edu>
2020-02-17 05:05:23 +0000

    lab 3 sample menu

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.lab3milestone1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +31,23 @@ public class MainActivity extends AppCompatActivity {
         ExampleDialog exampleDialog = new ExampleDialog();
         exampleDialog.show(getSupportFragmentManager(),"example dialog");
 
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this,"Item 2 selected",Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:return super.onOptionsItemSelected(item);
+        }
     }
 }
